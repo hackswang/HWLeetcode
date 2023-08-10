c3a2ebe35c847fa0993ab785a6e4513edf771f65
wangzihe <wangzihe@shizhuang-inc.com>
2023-08-10 14:31:48 +0800

    Coding - 168、169、171

---
+++ C++/_0500/_0200/0171_excel-sheet-column-number.cpp
@@ -0,0 +1,86 @@
+#include <iostream>
+
+using namespace std;
+
+// https://leetcode.cn/problems/excel-sheet-column-number/
+
+
+/*
+给你一个字符串 columnTitle ，表示 Excel 表格中的列名称。返回 该列名称对应的列序号 。
+
+例如：
+
+A -> 1
+B -> 2
+C -> 3
+...
+Z -> 26
+AA -> 27
+AB -> 28 
+...
+ 
+
+示例 1:
+
+输入: columnTitle = "A"
+输出: 1
+
+
+示例 2:
+
+输入: columnTitle = "AB"
+输出: 28
+
+
+示例 3:
+
+输入: columnTitle = "ZY"
+输出: 701
+ 
+
+提示：
+
+1 <= columnTitle.length <= 7
+columnTitle 仅由大写英文组成
+columnTitle 在范围 ["A", "FXSHRXW"] 内
+*/
+
+
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int result = 0;
+        for (int i=0; i<columnTitle.size(); i++) {
+            result = result * 26 + (columnTitle[i] - 'A' + 1);
+        }
+        return result;
+    }
+};
+
+int main() {
+
+    Solution solution;
+    {
+        int result = solution.titleToNumber("A");
+        cout << endl;
+    }
+    {
+        int result = solution.titleToNumber("AB");
+        cout << endl;
+    }
+    {
+        int result = solution.titleToNumber("ZY"); // 701
+        cout << endl;
+    }
+    {
+        int result = solution.titleToNumber("AAY"); // 727
+        cout << endl;
+    }
+    {
+        int result = solution.titleToNumber("FXSHRXW"); // 2147483647
+        cout << endl;
+    }
+    cout << "end";
+
+    return 0;
+}
